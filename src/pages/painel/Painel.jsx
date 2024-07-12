21e56bd52f9b70e9ed49c98c2fe413597d1f6dd9
Silvana Paiva <sil_paiva@hotmail.com>
2024-07-12 01:13:07 -0300

    add carrossel da recepcao

--- src/pages/painel/Painel.jsx
+++ src/pages/painel/Painel.jsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Grid } from '@mui/material';
 import FloorTab from '../../components/tabelaAndar/tabelaAndar';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 import './Painel.css';
 import imagem1 from '../../imagens/testeTela/di2win.png';
 import imagem2 from '../../imagens/logos/LogoSoftex.png';
@@ -18,14 +20,32 @@ import imagem13 from '../../imagens/testeTela/iLand_logo.png'
 import imagem14 from '../../imagens/testeTela/consenso.png'
 import imagem15 from '../../imagens/testeTela/spacemidia.jpeg'
 import imagem16 from '../../imagens/testeTela/elogica_logo.png'
+import imagemSF from '../../imagens/testeTela/sem_foto.png'
 
 export default function Painel() {
     const mezzaninoItems = [
+      {
+        img: imagemSF,
+        title: 'Sala de Reunião',
+        author: '001',
+      },
+      {
+        img: imagemSF,
+        title: 'Sala de Reunião',
+        author: '002',
+      },
+      {
+        img: imagemSF,
+        title: 'Auditório',
+        author: '003',
+      },
+      
         {
             img: imagem3,
             title: 'UniBTA',
-            author: '003',
+            author: '004',
           },
+          
     ];
     const mezzaninoSalas = [
       {
@@ -46,9 +66,59 @@ export default function Painel() {
             author: '102',
           },
           {
+            img: imagemSF,
+            title: 'Empresa x',
+            author: '103',
+          },
+          {
+            img: imagemSF,
+            title: 'Empresa XX',
+            author: '104',
+          },
+          {
+            img: imagemSF,
+            title: 'Empresa Xy',
+            author: '105',
+          },
+          {
             img: imagem2,
-            title: 'Softex PE',
+            title: 'Softex Coworking',
             author: '106',
+          },
+          {
+            img: imagemSF,
+            title: 'Joy Education',
+            author: '106.02',
+          },
+          {
+            img: imagemSF,
+            title: 'Agil Média Tensão',
+            author: '106-04',
+          },
+          {
+            img: imagemSF,
+            title: 'L.a.',
+            author: '106.05',
+          },
+          {
+            img: imagemSF,
+            title: 'Neuron Predict',
+            author: '106.06',
+          },
+          {
+            img: imagemSF,
+            title: 'MedPerícia',
+            author: '106.07',
+          },
+          {
+            img: imagemSF,
+            title: 'Monitore',
+            author: '106.08',
+          },
+          {
+            img: imagemSF,
+            title: 'IncubaTic',
+            author: '106.09',
           },
           {
             img: imagem5,
@@ -152,7 +222,7 @@ export default function Painel() {
         { title: "6º Andar", items: sextoAndarItems },
       ];
 
-      return (
+      /*return (
         <>
         <div >
         <Grid container spacing={1} className='painelRecepcao'>
@@ -164,6 +234,34 @@ export default function Painel() {
       </Grid>
       </div>
       </>
-      );
+      );*/
+      return (
+        <div className="painelRecepcao">
+          <Carousel 
+            autoPlay 
+            interval={4500} 
+            infiniteLoop 
+            showThumbs={false}
+            showStatus={false}
+            showArrows={false}
+          >
+            
+            {andares.map((andar, index) => (
+                    <div key={index}>
+                        <div className="andar-title-container">
+                            <h2 className="andar-title">{andar.title}</h2>
+                        </div>
+                        <Grid container spacing={1}>
+                            {andar.items.map((item, idx) => (
+                                <Grid key={idx} item xs={12} sm={6} md={4} lg={2} xl={1}>
+                                    <FloorTab  items={[item]} salas={andar.salas} />
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </div>
+                ))}
+            </Carousel>
+        </div>
+    );
     }
 
